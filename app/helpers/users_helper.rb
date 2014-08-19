a3c993c1b8637b4171f4ebd8b2584cf9e173c2de
Liam Speirs <liamspeirs1@hotmail.com>
2014-08-18 21:20:12 -0400

    Fix?

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
 	def fetchcardsintodb
 		$magic = "http://api.mtgdb.info/cards/"
 		response = Net::HTTP.get_response(URI.parse($magic))
-		data = response.body
+		data = begin IO.read('public/cards.json') rescue "" end
 		arr = JSON.parse(data, opts = {:symbolize_names => true}    )
 		arr[0,1000].each do |hash|
 		Card.create(:mId => hash[:id], :relatedCardId => hash[:relatedCardId], :setNumber => hash[:setNumber], :name => hash[:name], :searchName => hash[:searchName], :description => hash[:description], :flavor => hash[:flavor], :colors => hash[:colors], :manaCost => hash[:manaCost], :convertedManaCost => hash[:convertedManaCost], :cardSetName => hash[:cardSetName], :mtgType => hash[:type], :subType => hash[:subType], :power => hash[:power],  :toughness => hash[:toughness], :loyalty => hash[:loyalty], :rarity => hash[:rarity], :artist => hash[:artist], :cardSetId =>  hash[:cardSetId], :token => hash[:token], :promo => hash[:promo], :releasedAt => hash[:releasedAt])
